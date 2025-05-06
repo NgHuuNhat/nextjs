24761000bb55ed7dbc372696db0fb8c247b6a16a
Ng Huu Nhat <nhat200901@gmail.com>
2025-05-06 21:34:10 +0700

    update

--- my-next-app/src/app/product/services/api.ts
+++ my-next-app/src/app/product/services/api.ts
@@ -1,19 +1,21 @@
 import { Product } from "../types"
 
+//ISR
 export async function getProducts(): Promise<Product[]> {
     const res = await fetch(`http://localhost:3000/api/product`, {
         // cache: "no-store",  // đảm bảo fetch mới nhất mỗi lần load (không cache)
-        next: { revalidate: 60 }, // SEO: ISR sau mỗi 60s
+        next: { revalidate: 600 }, // SEO: ISR sau mỗi 600s
     })
 
     if (!res.ok) return []
     return res.json()
 }
 
+//SSR
 export async function getProductBySlug(slug: string): Promise<Product | null> {
     const res = await fetch(`http://localhost:3000/api/product/${slug}`, {
-        // cache: "no-store",  // đảm bảo fetch mới nhất mỗi lần load (không cache)
-        next: { revalidate: 60 }, // SEO: ISR sau mỗi 60s
+        cache: "no-store",  // đảm bảo fetch mới nhất mỗi lần load (không cache)
+        // next: { revalidate: 60 }, // SEO: ISR sau mỗi 60s
     })
 
     if (!res.ok) return null
